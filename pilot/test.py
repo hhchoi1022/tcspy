b3854e3ec794fddea16958802f42e7af0d3b922b
HyeonhoChoi <hhchoi1022@gmail.comi>
2023-02-25 18:55:28 +0900

    2023년 2월 25일 토요일 17시 43분 38초 KST

--- pilot/test.py
+++ pilot/test.py
@@ -2,42 +2,28 @@
 
 #%%
 from tcspy.utils.to_skycoord import to_SkyCoord
-
-from alpaca.camera import Camera
-from alpaca.focuser import Focuser
-from alpaca.filterwheel import FilterWheel
-from alpaca.telescope import Telescope
-
 from tcspy.pilot.startup import StartUp
-from tcspy.utils import mainLogger
 from tcspy.utils.target import mainTarget
-from tcspy.configuration import mainConfig
-from tcspy.devices.camera import mainCamera
-from tcspy.devices.focuser import mainFocuser
-from tcspy.devices.telescope import mainTelescope_Alpaca
-from tcspy.devices.filterwheel import mainFilterwheel
-from tcspy.devices.observer import mainObserver
-
 from tcspy.pilot.singleobservation import singleObservation
-#%% Setting devices
+#%% Startup
 startup = StartUp()
 startup.run()
 devices = startup.devices
 observer = startup.observer
-#%%
-obs = singleObservation(**devices, observer = observer)
-#%%
-ra = '12:39:30.7'
-dec = '-26:45:00'
-target_name = 'M68'
+#%% Setting Target
+ra = '17:37:36'
+dec = '-03:14:59'
+target_name = 'M14'
 coord_radec = to_SkyCoord(ra, dec)
 ra_hour ,dec_deg = coord_radec.ra.hour, coord_radec.dec.deg
 alt = 60
 az = 170
 target = mainTarget(observer = observer, target_ra = ra_hour, target_dec = dec_deg, target_name = target_name)
 target.staralt()
-#%%
-#obs.slew_exposure(target_alt = alt, target_az = az, exptime = 10, binning = 1, target_name = target_name)
-image = obs.slew_exposure(target_ra = ra_hour, target_dec = dec_deg, exptime = 60, binning = 1, target_name = target_name)
+#%% Observation
+obs = singleObservation(**devices, observer = observer)
+image = obs.slew_exposure(target_ra = ra_hour, target_dec = dec_deg, exptime = 120, binning = 1, target_name = target_name)
 image.show()
-image.save('{}_3.fits'.format(target_name))
+image.save('{}_1.fits'.format(target_name))
+devices['telescope'].park()
+
