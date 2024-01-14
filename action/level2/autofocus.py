ba2a83761a5ae1b81d7031fafa1a8842bc07321b
linux <hhchoi1022@gmail.com>
2024-01-14 22:22:42 +0900

    240114_lnx

--- action/level2/autofocus.py
+++ action/level2/autofocus.py
@@ -7,6 +7,13 @@ from astropy.io import fits
 import re
 import numpy as np
 from scipy.ndimage import convolve
+from photutils.segmentation import detect_threshold, detect_sources
+from astropy.stats import sigma_clipped_stats, SigmaClip
+from photutils.utils import circular_footprint
+from photutils.detection import DAOStarFinder
+from astropy.nddata import Cutout2D
+import matplotlib.pyplot as plt
+
 #%%
 dir_key = '/home/hhchoi1022/Desktop/data2/Autofocus/Test_samples/FocusTest*'
 folder_list = glob.glob(dir_key)
@@ -18,6 +25,112 @@ for tel, folder in zip(tel_list, folder_list):
     folder_dict[tel]['folder'] = folder
     filelist = glob.glob(folder + '/*.fits')
     folder_dict[tel]['files'] = sorted(filelist)
+
+#%%
+
+import cv2
+def fparam(data):
+        # compute the Laplacian of the image and then return the focus
+        # measure, which is simply the variance of the Laplacian
+        return cv2.Laplacian(data, cv2.CV_64F).var()
+focus_files = folder_dict['7DT01']['files']
+test_files = [focus_files[i] for i in [5,10,15,20,25,30,35]]
+test_files = focus_files
+sum_lap_subsequent = []
+for focus_file in test_files:
+    #focus_file = focus_files[0]    
+    data = fits.getdata(focus_file)
+    """
+    start = time.perf_counter()
+    sigma_clip = SigmaClip(sigma=50.0, maxiters=1)
+    threshold = detect_threshold(data, nsigma=10, sigma_clip=sigma_clip)
+    segment_img = detect_sources(data, threshold, npixels=10)
+    sources = [segment_img.bbox[i].center for i in range(len(segment_img.bbox)) if (segment_img.bbox[i].center[0] > 300) &  (segment_img.bbox[i].center[0] < 6388-300) &  (segment_img.bbox[i].center[1] > 300) &  (segment_img.bbox[i].center[1] < 9576-300) ]
+    patch_size = 100  # Set your desired patch size
+
+
+    for source in sources[:10]:
+        center = (source[1], source[0])
+        
+        # Create a Cutout2D object to extract the rectangular patch
+        cutout = Cutout2D(data, center, size=(patch_size, patch_size), mode='partial', fill_value=np.nan)
+
+        # Display the patch
+        plt.imshow(cutout.data, origin='lower', cmap='viridis', interpolation='nearest', label = fparam(cutout.data))
+        plt.title(fparam(cutout.data))
+        plt.show()
+
+    print(time.perf_counter() - start)
+    """
+    array_shape = data.shape
+    tile_size = 1500
+    tiles_per_side = 2
+    # Calculate the center region
+    center_start_row = (array_shape[0] - tile_size * tiles_per_side) // 2
+    center_start_col = (array_shape[1] - tile_size * tiles_per_side) // 2
+
+    # Initialize an empty list to store tiles
+    tiles = []
+
+    # Iterate through rows and columns to extract tiles
+    for i in range(tiles_per_side):
+        for j in range(tiles_per_side):
+            start_row = center_start_row + i * tile_size
+            start_col = center_start_col + j * tile_size
+            tile = data[start_row:start_row + tile_size, start_col:start_col + tile_size]
+            tiles.append(tile)
+
+    # Display one of the tiles (for demonstration purposes)
+    sum_lap = 0
+    sum_lap_list = []
+    for tile in tiles:
+        sum_lap_list.append(fparam(tile))
+        #sum_lap += fparam(tile)
+    #plt.plot(sum_lap_list)
+    sum_lap_list = np.array(sum_lap_list)
+    indices_of_largest = np.argpartition(sum_lap_list, -3)[-3:]
+    sum_lap_subsequent.append(np.sum(sum_lap_list[indices_of_largest]))
+    
+#%%
+def calc_norm_variance(array):
+    
+    mean = np.mean(array)
+    variance = np.sum((np.array(array) - mean)**2)
+    FM = 1/(array.shape[0]*array.shape[1]) * variance / mean
+    return FM
+#%%
+test_files = [focus_files[i] for i in [5,10,15,20,25,30,35]]
+all_FM = []
+for file in test_files:
+    data = fits.getdata(file)
+    all_FM.append((calc_norm_variance(data)))
+#%%
+plt.plot(all_FM)
+#%%
+plt.plot(sum_lap_subsequent)
+#%%
+
+#%%
+
+#%%
+
+#%%
+for source in sources:
+    center = (source['xcentroid'], source['ycentroid'])
+    
+    # Create a Cutout2D object to extract the rectangular patch
+    cutout = Cutout2D(data, center, size=(patch_size, patch_size), mode='partial', fill_value=np.nan)
+
+    # Display the patch
+    plt.imshow(cutout.data, origin='lower', cmap='viridis', interpolation='nearest')
+    plt.title('Source Patch')
+    plt.show()
+#footprint = circular_footprint(radius=10)
+#mask = segment_img.make_source_mask(footprint=footprint)
+
+#mean, median, std = sigma_clipped_stats(data, sigma=3.0, mask=mask)
+#%%
+data
 #%%
 i = 20
 for tel, info in folder_dict.items():
@@ -75,4 +188,72 @@ import matplotlib.pyplot as plt
 f, axes = plt.subplots(2,1, figsize = (25,25))
 axes[0].imshow(bkg, vmin = 510, vmax = 520)
 axes[1].imshow(data, vmin = 510, vmax = 520)
-# %%
+
+#%%
+
+def crop_image(data, 
+               center_x : int,
+               center_y : int,
+               crop_size : int):
+    x_start = max(0, center_x - crop_size // 2)
+    x_end = min(data.shape[1], center_x + crop_size // 2)
+    y_start = max(0, center_y - crop_size // 2)
+    y_end = min(data.shape[0], center_y + crop_size // 2)
+    data_cropped = data[y_start:y_end, x_start:x_end]
+    return data_cropped
+#%%
+from astropy.io import fits
+from astropy.modeling import models, fitting
+from astropy.stats import sigma_clipped_stats
+from photutils import DAOStarFinder, aperture_photometry, CircularAperture
+
+saturation_level = 3000
+your_snr_threshold = 20
+
+# Step 1: Read FITS file
+hdulist = fits.open(focus_files[0])
+data = hdulist[0].data
+#%%
+crop_data = crop_image(data, data.shape[1]//2, data.shape[0]//2, 3000)
+
+plt.imshow(crop_data, vmin = 515, vmax = 540)
+#%%
+plt.imshow(data, vmin = 515, vmax = 540)
+
+#%%
+# Step 2: Source Detection
+mean, median, std = sigma_clipped_stats(crop_data, sigma=3.0)
+daofind = DAOStarFinder(fwhm=4, threshold=20 * std)
+sources = daofind(crop_data - median)
+#%%
+# Step 3: Calculate SNR and Check Saturation
+sources['snr'] = sources['peak'] / std
+saturation_mask = sources['peak'] < saturation_level
+
+# Step 4: Combine SNR and Saturation Checks
+valid_sources_mask = sources['snr'] > your_snr_threshold  # Adjust your_snr_threshold as needed
+valid_sources_mask &= saturation_mask
+valid_sources = sources[valid_sources_mask]
+#%%
+# Step 5: Sort by SNR
+valid_sources.sort('snr', reverse = True)
+
+# Step 6: Select Top 30 Valid Sources
+top_30_valid_sources = valid_sources[:30]
+
+# Step 7: Calculate FWHM
+fwhm_values = []
+#%%
+x, y = np.meshgrid(np.arange(0, 20, 1), np.arange(0, 20, 1))
+from photutils.aperture import ApertureStats
+#%%
+for source in top_30_valid_sources:
+    position = (source['xcentroid'], source['ycentroid'])
+    gauss_init = models.Gaussian2D(amplitude=source['peak'],
+                                   x_mean=10, y_mean=10)
+    fit_data = crop_image(crop_data, int(position[0]), int(position[1]), 20)
+    fitter = fitting.LevMarLSQFitter()
+    gauss_fit = fitter(gauss_init, x,y, fit_data)
+        # Extract FWHM
+    fwhm = gauss_fit.x_stddev
+#%%
