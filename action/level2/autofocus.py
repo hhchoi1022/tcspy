eaf9663dffeeb570f3c40fa548e196f9f52ed85c
HyeonhoChoi <hhchoi1022@gmail.comi>
2024-01-15 13:39:30 +0900

    2024년 1월 15일 월요일 13시 39분 17초 KST

--- action/level2/autofocus.py
+++ action/level2/autofocus.py
@@ -13,21 +13,202 @@ from photutils.utils import circular_footprint
 from photutils.detection import DAOStarFinder
 from astropy.nddata import Cutout2D
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from astropy.io import fits
+from astropy.modeling import models, fitting
+from astropy.stats import sigma_clipped_stats
+from photutils import DAOStarFinder, aperture_photometry, CircularAperture
+
+
+
+
 
 #%%
-dir_key = '/home/hhchoi1022/Desktop/data2/Autofocus/Test_samples/FocusTest*'
-folder_list = glob.glob(dir_key)
+file_key = '/Users/hhchoi1022/code/FocusTest_240112_7DT01/*.fits'
+files = glob.glob(file_key)
 #%%
-tel_list =  [re.findall('(7DT\d\d)',folder)[0] for folder in folder_list]
-folder_dict = dict()
-for tel, folder in zip(tel_list, folder_list):
-    folder_dict[tel] = dict()
-    folder_dict[tel]['folder'] = folder
-    filelist = glob.glob(folder + '/*.fits')
-    folder_dict[tel]['files'] = sorted(filelist)
+file = files[3]
+
 
 #%%
 
+
+def crop_image(data, 
+               center_x : int,
+               center_y : int,
+               crop_size : int):
+    x_start = max(0, center_x - crop_size // 2)
+    x_end = min(data.shape[1], center_x + crop_size // 2)
+    y_start = max(0, center_y - crop_size // 2)
+    y_end = min(data.shape[0], center_y + crop_size // 2)
+    data_cropped = data[y_start:y_end, x_start:x_end]
+    return data_cropped
+
+
+
+
+
+
+
+
+#%% Visualization of the cropped image
+
+
+crop_size = 5000
+data = fits.getdata(file)
+data_mean = np.mean(data)
+data_std = np.std(data)
+data_cropped = crop_image(data, data.shape[1]//2, data.shape[0]//2, crop_size)
+data_c_mean = np.mean(data_cropped)
+data_c_std = np.std(data_cropped)
+fig, axes = plt.subplots(2, figsize = (30,20))
+axes[0].imshow(data, vmin = data_mean-data_std, vmax = data_mean + data_std)
+square = patches.Rectangle(((data.shape[1]//2)-crop_size//2, (data.shape[0]//2)-crop_size//2), crop_size, crop_size, linewidth=3, edgecolor='r', facecolor='none')
+axes[0].add_patch(square)
+axes[1].imshow(data_cropped, vmin = data_mean-data_std, vmax = data_mean + data_std)
+#%%
+#%%
+mean, median, std = sigma_clipped_stats(data_cropped, sigma=3.0)
+daofind = DAOStarFinder(fwhm=4, threshold=20 * std)
+sources = daofind(data_cropped - median)
+#%%
+'''sources['snr'] = sources['peak'] / std
+saturation_mask = sources['peak'] < saturation_level
+
+# Step 4: Combine SNR and Saturation Checks
+valid_sources_mask = sources['snr'] > your_snr_threshold  # Adjust your_snr_threshold as needed
+valid_sources_mask &= saturation_mask
+valid_sources = sources[valid_sources_mask]'''
+#%%
+from scipy import optimize, ndimage
+center = (sources[30]['xcentroid'], sources[30]['ycentroid'])
+
+# Create a Cutout2D object to extract the rectangular patch
+cutout = Cutout2D(data_cropped, center, size=(20, 20), mode='partial', fill_value=np.nan)
+data_c_mean = np.mean(cutout.data)
+data_c_std = np.std(cutout.data)
+plt.imshow(cutout.data, vmin = data_mean-data_std, vmax = data_mean + data_std)
+#%%
+#data_cropped = cutout.data
+def _window_function(arr, border=0):
+
+    """
+    Creates a sine window function of the same size as some 1-D array "arr".
+    Optionally, a zero border at the edges is added by "scrunching" the window.
+    """
+    ndata = len(arr)
+    nwind = ndata - 2 * border
+
+    w = np.zeros(ndata)
+    for i in range(nwind):
+        w[i + border] = np.sin(np.pi * (i + 1.) / (nwind + 1.))
+    return w
+def _fit_correlation(correl):
+    from lmfit.models import GaussianModel
+
+    # create Gaussian model
+    model = GaussianModel()
+
+    # initial guess
+    x = np.arange(len(correl))
+    pars = model.guess(correl, x=x)
+    pars['sigma'].value = 20.
+
+    # fit
+    return model.fit(correl, pars, x=x)
+
+def _clean(data, backsub=True, xbad=None, ybad=None):
+    """
+    Removes global slopes and fills up bad rows (ybad) or columns (xbad).
+    """
+    (ny, nx) = data.shape
+
+    # REMOVE BAD COLUMNS AND ROWS
+    if xbad is not None:
+        x1 = xbad - 1
+        if x1 < 0:
+            x1 = 1
+        x2 = x1 + 2
+        if x2 >= nx:
+            x2 = nx - 1
+            x1 = x2 - 2
+        for j in range(ny):
+            data[j][xbad] = 0.5 * (data[j][x1] + data[j][x2])
+    if ybad is not None:
+        y1 = ybad - 1
+        if y1 < 0:
+            y1 = 1
+        y2 = y1 + 2
+        if y2 >= ny:
+            y2 = ny - 1
+            y1 = y2 - 2
+        for i in range(nx):
+            data[ybad][i] = 0.5 * (data[y1][i] + data[y2][i])
+
+    # REMOVE GLOBAL SLOPES
+    if backsub:
+        xsl = np.median(data, axis=0)
+        ysl = np.median(data, axis=1).reshape((ny, 1))
+        xsl -= np.mean(xsl)
+        ysl -= np.mean(ysl)
+        xslope = np.tile(xsl, (ny, 1))
+        yslope = np.tile(ysl, (1, nx))
+        return data - xslope - yslope
+    else:
+        return data
+
+def _analyse_image(data, backsub=True, xbad=None, ybad=None):
+    # clean data
+    data = _clean(data, backsub=backsub, xbad=xbad, ybad=ybad)
+
+    # get projections
+    xproj = np.mean(data, axis=0)  # PROJECTIONS
+    yproj = np.mean(data, axis=1)
+    nx = len(xproj)
+    ny = len(yproj)
+
+    # remove background gradient
+    xclean = xproj - ndimage.uniform_filter1d(xproj, nx // 10)
+    yclean = yproj - ndimage.uniform_filter1d(yproj, ny // 10)
+
+    # get window functions
+    xwind = _window_function(xclean, border=3)
+    ywind = _window_function(yclean, border=3)
+
+    # calculate correlation functions
+    xavg = np.average(xclean)
+    yavg = np.average(yclean)
+    x = xwind * (xclean - xavg) / xavg
+    y = ywind * (yclean - yavg) / yavg
+    xcorr = np.correlate(x, x, mode='same')
+    ycorr = np.correlate(y, y, mode='same')
+
+    # filter out the peak (e.g. cosmics, ...)
+    # imx = np.argmax(xcorr)
+    # xcorr[imx] = 0.5 * (xcorr[imx - 1] + xcorr[imx + 1])
+    # imx = np.argmax(ycorr)
+    # ycorr[imx] = 0.5 * (ycorr[imx - 1] + ycorr[imx + 1])
+
+    # fit cc functions to get fwhm
+    xfit = _fit_correlation(xcorr)
+    yfit = _fit_correlation(ycorr)
+
+    # log it
+    print('Found x=%.1f+-%.1f and y=%.1f+-%.1f.'%(xfit.params['fwhm'].value, xfit.params['fwhm'].stderr,yfit.params['fwhm'].value, yfit.params['fwhm'].stderr))
+
+
+#%%
+
+_analyse_image(data = data_cropped)
+#%%
+
+yfit.params
+
+#%%
+xfit.params
+#%%
+plt.plot(xcorr)
+#%%
 import cv2
 def fparam(data):
         # compute the Laplacian of the image and then return the focus
@@ -190,22 +371,8 @@ axes[0].imshow(bkg, vmin = 510, vmax = 520)
 axes[1].imshow(data, vmin = 510, vmax = 520)
 
 #%%
-
-def crop_image(data, 
-               center_x : int,
-               center_y : int,
-               crop_size : int):
-    x_start = max(0, center_x - crop_size // 2)
-    x_end = min(data.shape[1], center_x + crop_size // 2)
-    y_start = max(0, center_y - crop_size // 2)
-    y_end = min(data.shape[0], center_y + crop_size // 2)
-    data_cropped = data[y_start:y_end, x_start:x_end]
-    return data_cropped
+data
 #%%
-from astropy.io import fits
-from astropy.modeling import models, fitting
-from astropy.stats import sigma_clipped_stats
-from photutils import DAOStarFinder, aperture_photometry, CircularAperture
 
 saturation_level = 3000
 your_snr_threshold = 20
