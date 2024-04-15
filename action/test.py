fd262351092d2efff1feccff043f5f553bb63e70
mcs <hhchoi1022@gmail.com>
2024-04-15 13:41:20 +0900

    240415_linux

--- action/test.py
+++ action/test.py
@@ -4,105 +4,56 @@ from threading import Event
 from tcspy.devices import SingleTelescope
 abort_action = Event()
 from tcspy.devices.camera import mainCamera
+from tcspy.action import MultiAction
+
+#%%
+
+
+
+def return_image(cam):
+    a = cam.device.ImageArra
+    return a
+#%%
+from threading import Thread
+
+unitnumlist = [1,2,3,5,6,7,8,9,10,11]
+for unitnum in unitnumlist:
+    Thread(target = return_image, kwargs = dict(cam = mainCamera(unitnum))).start()
+#%%
+a1.start()
+a2.start()
+a3.start()
+a5.start()
+a6.start()
+a7.start()
+a8.start()
+a9.start()
+a10.start()
+a11.start()
+
+#%%
+
+
 #%%
 telescope1 = SingleTelescope(1)
 telescope2 = SingleTelescope(2)
 telescope3 = SingleTelescope(3)
-telescope = SingleTelescope(1)
-#cam1 = mainCamera(1)
-cam2 = mainCamera(2)
-cam3 = mainCamera(3)
-cam5 = mainCamera(5)
-def return_image(cam):
-    
-    start = time.time()
-    a = cam.device.ImageArray
-    print(cam.unitnum,  time.time() -start)
-    return a
-#%%
-from threading import Thread
-import time
-kwargs2 = dict(cam = cam2)
-kwargs3 = dict(cam = cam3)
-kwargs5 = dict(cam = cam5)
-a = Thread(target  =return_image, kwargs = kwargs2)
-b = Thread(target  =return_image, kwargs = kwargs3)
-c = Thread(target  =return_image, kwargs = kwargs5)
-#%%
-a.start()
-b.start()
-c.start()
-a.join()
-#%%
-import requests
-import threading
-def make_request(url):
-    response = requests.get(url)
-    print(f"Response from {url}: {response.status_code}")
-# List of URLs to make requests to
-urls = [
-    "https://www.example.com",
-    "https://www.google.com",
-    "https://www.wikipedia.org",
-    "https://www.python.org"
-]
-
-# Create and start threads for each URL
-threads = []
-for url in urls:
-    thread = threading.Thread(target=make_request, args=(url,))
-    thread.start()
-    threads.append(thread)
-# Wait for all threads to finish
-#%%
-import time
-from concurrent.futures import ThreadPoolExecutor
-
-# Define the function to retrieve an image
-def return_image(cam):
-    start = time.time()
-    a = cam.device.ImageArray
-    print(cam.unitnum, time.time() - start)
-    return a
-
-# Create mainCamera instances
-cam2 = mainCamera(2)
-cam3 = mainCamera(3)
-cam5 = mainCamera(5)
-#%%
-# Create a ThreadPoolExecutor with 3 threads
-with ThreadPoolExecutor(max_workers=10) as executor:
-    # Submit the return_image function with arguments to the executor
-    futures = [executor.submit(return_image, cam) for cam in [cam2, cam3, cam5]]
-
-    # Wait for all tasks to complete
-    for future in futures:
-        future.result()
-
-#%%
-from threading import Thread
-import time
-
-kwargs = dict(abort_action = Event(), exptime = 5, imgtype = 'Dark', binning = 1, is_light = False, gain =0)
-#a = Thread(target  =cam1.exposure, kwargs = kwargs).start()
-a = Thread(target  =cam2.exposure, kwargs = kwargs).start()
-a = Thread(target  =cam3.exposure, kwargs = kwargs).start()
-a = Thread(target  =cam5.exposure, kwargs = kwargs).start()
-
-#%%
-
-
-#%%
-from tcspy.action import MultiAction
-array_telescope= [telescope1, telescope2, telescope3]
+telescope5 = SingleTelescope(5)
+telescope6 = SingleTelescope(6)
+telescope7 = SingleTelescope(7)
+telescope8 = SingleTelescope(8)
+telescope9 = SingleTelescope(9)
+array_telescope= [ telescope1, telescope2, telescope3]#, telescope5, telescope6, telescope7, telescope8, telescope9]
 kwargs = dict(frame_number = 1, exptime = 1, filter_ = 'r', imgtype = 'DARK')
 array_kwargs = []
-for i in range(3):
+for i in range(1):
     array_kwargs.append(kwargs)
-
-m = MultiAction(array_telescope = array_telescope, array_kwargs= array_kwargs, function = Exposure, abort_action = abort_action)
 #%%
+m = MultiAction(array_telescope = array_telescope, array_kwargs= kwargs, function = Exposure, abort_action = abort_action)
 m.run()
+#%%
+from tcspy.devices import MultiTelescopes
+mtel = MultiTelescopes(SingleTelescope_list= array_telescope)
 #%%
 #%%
 ChangeFilter(telescope, abort_action= abort_action).run('m425')
@@ -145,10 +96,29 @@ telescope.filterwheel.get_status()
 # %%
 SingleObservation(telescope, abort_action).run(10, 1, 'i', ra =300, dec = -22, autofocus_before_start = True)
 # %%
-telescope1 = SingleTelescope(1)
-telescope2 = SingleTelescope(2)
-telescope3 = SingleTelescope(3)
-telescope_array = [telescope1, telescope2, telescope3]
+from tcspy.action.level2 import * 
+telescope_array = []
+SingleObs_list = []
+
+for unitnum in [1,2,3,5,6,7,8,9,10,11]:
+    tel = SingleTelescope(unitnum)
+    SingleObs_list.append(SingleObservation(tel, Event()))
+#%%
+
+kwargs = dict( exptime = 10, count = 1, filter_ = 'i', alt =60, az = 300, autofocus_before_start = False)
+from concurrent.futures import ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=100) as executor:
+    # Submit the return_image function with arguments to the executor
+    futures = [executor.submit(SingleObs.run, **kwargs) for SingleObs in SingleObs_list]
+
+    # Wait for all tasks to complete
+    for future in futures:
+        future.result()
+#m = MultiAction(array_telescope= telescope_array, array_kwargs = kwargs, function = SingleObservation, abort_action= Event())
+#%%
+SingleObservation()
+#%%
+m.run()
 # %%
 from tcspy.devices import TelescopeStatus
 TelescopeStatus(telescope1).dict
