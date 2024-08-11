5a9150a26da3d943224903cfcf3278797b261598
linux <hhchoi1022@gmail.com>
2024-08-11 20:34:21 +0900

    240811_linux

--- utils/databases/GS_connector.py
+++ utils/databases/GS_connector.py
@@ -186,17 +186,19 @@ class GoogleSheet():
             worksheet.update([header])
 # %%
 if __name__ =='__main__':
-    tbl = ascii.read('/home/hhchoi1022/Downloads/calspec_2024-07-20.csv')
+    tbl = ascii.read('/home/hhchoi1022/Downloads/calspec_2024-08-08.csv')
     tbl.rename_column('Star name', 'objname')
     tbl.rename_column('Decl', 'De')
     tbl.rename_column('obs_count', 'count')
-    obscount_new = [obscount//2 for obscount in tbl['count']]
+    #obscount_new = [obscount//2 for obscount in tbl['count']]
+    exptime_new = [f'10,10,10,{exptime},{exptime},{exptime},{exptime}' for exptime in tbl['exptime']]
     tbl['obsmode'] = 'Spec'
     tbl['specmode'] = 'calspec'
     tbl['group'] = 1
     tbl['priority'] = 1 
     tbl['gain'] = 0
-    tbl['count'] = obscount_new
-    GoogleSheet().write_sheet_data(sheet_name = '20240808', data  = tbl, append = False)
-    
+    #tbl['count'] = obscount_new
+    tbl['exptime'] = exptime_new
+    GoogleSheet().write_sheet_data(sheet_name = '20240809', data  = tbl, append = False)
 # %%
+d
