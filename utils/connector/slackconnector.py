9840580ef0581eb92bdfaaf2cd437a83b3982311
mcs <hhchoi1022@gmail.com>
2024-11-30 08:32:18 -0300

    241130_linux

--- utils/connector/slackconnector.py
+++ utils/connector/slackconnector.py
@@ -5,14 +5,15 @@
 from slack_sdk import WebClient
 import os
 #%%
-class SlackConnector():
+class SlackConnector:
     
     def __init__(self,
-                 token_path : str = f'{os.path.expanduser("~")}/.config/slack/slack_token_7dt_obseration_alert.txt',
-                 default_channel_id : str = 'C07SREPTWFM'):
+                 token_path: str = f'{os.path.expanduser("~")}/.config/slack/slack_token_7dt_obseration_alert.txt',
+                 default_channel_id: str = 'C07SREPTWFM'):
         token = open(token_path, 'r').read()
-        self.client = WebClient(token = token)
+        self.client = WebClient(token=token)
         self.channel_id = default_channel_id
+        self.channel_name = self._get_channel_name(default_channel_id) if default_channel_id else None
 
     def __repr__(self):
         channel_info = f"Channel: {self.channel_name or 'No channel selected'}"
@@ -28,15 +29,13 @@ class SlackConnector():
         except Exception as e:
             print(f"Failed to fetch channel name for ID {channel_id}: {e}")
             return "Unknown Channel"
-    
+
     def get_channel_id(self, channel_name):
         """
-        get channel id based on the channel name
+        Get channel ID based on the channel name.
         """
-        # conversations_list() 
         private_channels = self.client.conversations_list(types="private_channel").data['channels']
         public_channels = self.client.conversations_list(types="public_channel").data['channels']
-        # channel information
         channels = private_channels + public_channels
         channel = list(filter(lambda c: c["name"] == channel_name, channels))
         
@@ -47,23 +46,20 @@ class SlackConnector():
     
     def select_channel(self, channel_name):
         """
-        select channel based on the channel name
+        Select a channel based on the channel name.
         """
         self.channel_id = self.get_channel_id(channel_name)
+        self.channel_name = channel_name
 
     def get_message_ts(self, match_string):
         """
-        슬랙 채널 내 메세지 조회
+        Retrieve a message timestamp in the selected Slack channel.
         """
         if self.channel_id is None:
             raise ValueError("Channel is not selected")
-        # conversations_history() 메서드 호출
         result = self.client.conversations_history(channel=self.channel_id)
-        # 채널 내 메세지 정보 딕셔너리 리스트
         messages = result.data['messages']
-        # 채널 내 메세지가 query와 일치하는 메세지 딕셔너리 쿼리
         matched_messages = list(filter(lambda m: match_string in m["text"], messages))
-        # 해당 메세지ts 파싱
         if len(matched_messages) == 0:
             print(f"Message {match_string} not found")   
             return None
@@ -72,24 +68,22 @@ class SlackConnector():
 
     def post_thread_message(self, message_ts, text):
         """
-        슬랙 채널 내 메세지의 Thread에 댓글 달기
+        Post a message to a thread in the selected Slack channel.
         """
         if self.channel_id is None:
             raise ValueError("Channel is not selected")
-        # chat_postMessage() 메서드 호출
         result = self.client.chat_postMessage(
             channel=self.channel_id,
-            text = text,
-            thread_ts = message_ts
+            text=text,
+            thread_ts=message_ts
         )
         print(f'Thread message posted: text = {text}')
         return result
 
     def post_message(self, text):
         """
-        슬랙 채널에 메세지 보내기
+        Post a message to the selected Slack channel.
         """
-        # chat_postMessage() 메서드 호출
         if self.channel_id is None:
             raise ValueError("Channel is not selected")
         result = self.client.chat_postMessage(
@@ -98,9 +92,3 @@ class SlackConnector():
         )
         print(f'Message posted: text = {text}')
         return result
-# %%
-if __name__ == '__main__':
-    A = SlackConnector()
-    #ts = A.get_message_ts('Test message')
-    #A.post_thread_message(A.channel_id, ts, 'Thread message')
-# %%
